3ed94fb32b266d8543c06c0916996aa9b159b4ac
christian <christian.cervantes.d@gmail.com>
2025-07-10 11:58:56 -0500

    first commit

---
+++ db/prisma.ts
@@ -0,0 +1,27 @@
+import { Pool, neonConfig } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaClient } from '@prisma/client';
+import type { Decimal } from '@prisma/client/runtime/library';
+import ws from 'ws';
+
+neonConfig.webSocketConstructor = ws;
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+
+export const prisma = new PrismaClient({ adapter }).$extends({
+  result: {
+    product: {
+      price: {
+        compute(product: { price: Decimal }) {
+          return product.price.toString();
+        },
+      },
+      rating: {
+        compute(product: { rating: Decimal }) {
+          return product.rating.toString();
+        },
+      },
+    },
+  },
+});
