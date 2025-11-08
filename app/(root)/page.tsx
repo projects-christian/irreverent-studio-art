7addc51757ec6bbe66cc5b6c5da7a26921768d6f
christian <christian.cervantes.d@gmail.com>
2025-11-08 14:45:14 -0500

    cleaning for eslint

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
-import _Slider from "@/components/shared/slider/slider";
+// import Slider from "@/components/shared/slider/slider";
 import ProductList from "@/components/shared/product/product-list";
 import { getLatestProducts } from "@/lib/actions/product.actions";
-import _Quote from "@/components/shared/quote/quote";
+// import Quote from "@/components/shared/quote/quote";
 import DesignOne from "@/components/shared/categories/design-one/categories-design-one";
 import HeroVideo from "@/components/home/hero-video/hero-video";
 import TopShopFeature from "@/components/shared/top-shop-feature/top-shop-feature";
