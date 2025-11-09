6429a71cb48b27e6a639231d761f3480c3d32c61
christian <christian.cervantes.d@gmail.com>
2025-11-09 13:13:07 -0500

    type product typescript

--- types/index.ts
+++ types/index.ts
@@ -3,7 +3,17 @@ import { insertProductSchema } from '@/lib/validators';
 
 export type Product = z.infer<typeof insertProductSchema> & {
   id: string;
+  name?: string;
+  slug?: string;
+  category?: string;
+  images?: string;
+  brand?: string;
+  description?: string;
+  stock?: number;
+  price?: number;
   rating: string;
   numReviews: number;
+  isFeatured?: boolean;
+  banner?: string;
   createdAt: Date;
 };
